d81daf21d5e2f2a6f6bcc535e0f67fc92034ada1
Jeong-mi <56066290+Jeong-mi@users.noreply.github.com>
2021-04-12 01:40:24 +0900

    ch6_06

---
+++ p282_06/app/src/main/java/com/example/p282_06/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.p282_06;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("6장 연습문제 282p_6번");
+
+    }
+}
